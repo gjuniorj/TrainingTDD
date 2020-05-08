8baa4bf22f80c3f24ab9dc6488c6c812ab659910
gjuniorj <gjuniorj@gmail.com>
2020-05-08 08:01:53 -0300

    mySerialize file created.

--- TDD-MySerialize.php
+++ TDD-MySerialize.php
@@ -2,7 +2,13 @@
 
 use PHPUnit\Framework\TestCase;
 
+require_once('mySerialize.php');
+
 class TDD_MySerialize extends TestCase
 {
-    
+
+    public function testCanSerializeNull()
+    {
+        $this->assertSame("N;",serializeNull(null));
+    }
 }
---
+++ mySerialize.php
@@ -0,0 +1,7 @@
+<?php
+
+
+function serializeNull()
+{
+    return "N;";
+}
